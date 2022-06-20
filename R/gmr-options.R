f581f4a7f8af82d8c8354af03f7c11c522ae677f
MatthVeron <mateoh.veron@gmail.com>
2022-06-20 10:52:03 -0700

    Fix name of vignette

--- R/gmr-options.R
+++ R/gmr-options.R
@@ -1,10 +1,10 @@
 #' The color-blind friendly palette with grey
 #'
 #' see http://www.cookbook-r.com/Graphs/Colors_%28ggplot2%29/
-#' 
+#'
 #' @author D'Arcy N. Webber
 #' @export
-#' 
+#'
 .cbPalette1 <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 
@@ -14,7 +14,7 @@
 #'
 #' @author D'Arcy N. Webber
 #' @export
-#' 
+#'
 .cbPalette2 <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 
@@ -28,15 +28,15 @@
 #' @param plot_cols the colour palette used for plotting (recommend the colour-blind palette cbPalette1 or cdPalette2)
 #' @author D'Arcy N. Webber
 #' @export
-#' 
+#'
 .gmr_options <- list(plot_type = "png", plot_resolution = 400,
                      plot_size = c(100,100), plot_cols = .cbPalette1,
                      thick = 2, thin = 1)
 
 #' Set plotting theme for ggplot2 via gmr
 #'
-#' Gives user control over plot theme by running ggplot2 functions 
-#' that do the same. This allows a user to set the theme without 
+#' Gives user control over plot theme by running ggplot2 functions
+#' that do the same. This allows a user to set the theme without
 #' independently loading the ggplot2 package.
 #'
 #' @param name of desired theme
@@ -44,10 +44,10 @@
 #' @export
 set_ggtheme <- function(theme){
   switch(theme,
-         bw      = ggtheme <<- theme_bw(),
-         gray    = ggtheme <<- theme_gray(),
-         classic = ggtheme <<- theme_classic(),
-         minimal = ggtheme <<- theme_minimal()
+         bw      = ggtheme <<- ggplot2::theme_bw(),
+         gray    = ggtheme <<- ggplot2::theme_gray(),
+         classic = ggtheme <<- ggplot2::theme_classic(),
+         minimal = ggtheme <<- ggplot2::theme_minimal()
   )
   message("The ggplot theme has been set to ", theme, " for this working session")
 }
